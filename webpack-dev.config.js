73b2d17e6ac24548888095a4f4569b8d59e3d600
Maurice van der Lee <mauricevanderlee@gmail.com>
2016-03-17 21:46:07 +0100

    Create develeopment and production
    Development: npm start
    Production: npm run build

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	entry: [
 		'webpack-dev-server/client?http://localhost:3000',
 		'webpack/hot/only-dev-server',
-		'bootstrap-loader/extractStyles',
+		'bootstrap-loader',
 		'./src/app',
 	],
 
